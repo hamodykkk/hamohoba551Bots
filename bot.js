6ac387728c3da2542617bee1beb8f7352c462d61
hamohoba551 <43608567+hamohoba551@users.noreply.github.com>
2018-11-03 13:43:10 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,8 @@
+
+client.on('ready', () => {
+
+    client.user.setGame(`اﻟشخص اﻟذي يحاول إﺳتفزازك يموت ﺑطريقه صاﻣته `,'https://www.twitch.tv/MeeRcY')
+
+});
+
+client.login("MzYxMjE3NjU4MTkwMDM2OTk5.Dr8UDg.fjkMFYs23Lk_ETeapVqnW1gpMYs");
