780b0a68f34d8439909192ec4d43614a94f597dd
hamohoba551 <43608567+hamohoba551@users.noreply.github.com>
2018-11-04 22:54:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,4 +13,17 @@ client.on('ready', () => {
 
 
 
+client.on("message", function(message) {
+    if(message.content.startsWith("ادخل الروم")) {
+       client.channels.get("505439693999964182").join()
+    }
+});
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
