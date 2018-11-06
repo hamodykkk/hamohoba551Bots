c744224f89a6de502054d45acb8687d2db812979
hamohoba551 <43608567+hamohoba551@users.noreply.github.com>
2018-11-06 17:56:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
 
-    client.user.setGame(`السعادة أن تجد روح تشبه روحك كثيراً`,'https://www.twitch.tv/MeeRcY')
+    client.user.setGame(`لا نراهم لكنهم يشاركوننا نفس الهواء`,'https://www.twitch.tv/MeeRcY')
 
 });
 
