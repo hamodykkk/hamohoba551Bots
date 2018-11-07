222a84afa20f7e71f9d3bd71c780176cf898b1ae
hamohoba551 <43608567+hamohoba551@users.noreply.github.com>
2018-11-07 23:39:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
 
-    client.user.setGame(`(: احذر مني لاني مجنون`,'https://www.twitch.tv/MeeRcY')
+    client.user.setGame(`أحياناً تبكي ولا تدري ما القصة غير أنك تعبت من كل شئ`,'https://www.twitch.tv/MeeRcY')
 
 });
 
