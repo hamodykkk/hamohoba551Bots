f531d81c94e0a1f82d12baf1f2e74f83b6d4f5bd
hamohoba551 <43608567+hamohoba551@users.noreply.github.com>
2018-11-10 20:52:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,5 +15,25 @@ client.on('ready', () => {
 
 
 
+client.on('ready',async () => {
+console.log("Starting..");
+let g = client.guilds.get("441998122827120640");
+let c = g.channels.get("505439693999964182");
+if(c.type === 'voice') {
+c.join();
+setInterval(() => {
+if(!g.me.voiceChannel) c.join();
+}, 1);
+} else {
+console.log("Failed To Join:\n The Channel Type isn't \"text\"");
+}
+});
+
+
+
+
+
+
+
 
 client.login(process.env.BOT_TOKEN);
