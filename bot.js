198ef589434aedbfc4e6d2b029647350c72c85fb
hamohoba551 <43608567+hamohoba551@users.noreply.github.com>
2018-11-21 19:16:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,20 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+
+const fs = require('fs')
 
 const client = new Discord.Client();
 
+const prefix = '';
+
 client.on('ready', () => {
+
+
+ 
+ console.log(`Logged in as [ Pharaohs]`);
+
+});
+
+
 
 
   const adminprefix = "";
